a77bcf680e9ee080fc358b9d433f758c8f40e7b6
Wan-Yifei <wanyifei0123@126.com>
2018-05-30 00:03:15 -0400

    refactor: Rewrite question#4,5,6

--- Test_AH/test.py
+++ Test_AH/test.py
@@ -22,9 +22,9 @@ def Bam_prepare(path_bam):
         py.sort('-o', '/home/ywan/localdata/test1/BioEng_Bioinformatics/Test_AH/sorted.bam', path_bam)
         py.index('/home/ywan/localdata/test1/BioEng_Bioinformatics/Test_AH/sorted.bam')
         path_bam = '/home/ywan/localdata/test1/BioEng_Bioinformatics/Test_AH/sorted.bam'
-        return(py.AlignmentFile(path_bam, 'rb'))
+        return(py.AlignmentFile(path_bam, 'rb'), path_bam)
         #print('Is there index: %s'%(samraw.check_index())) ## Check index    
-samraw = Bam_prepare(path_bam)
+samraw, path_bam = Bam_prepare(path_bam)
 
 # =============================================================================
 # Q1. Average sequencing quality
@@ -87,21 +87,25 @@ def Base_enrichment(temp,bam, target):
     
     Per_base = ontag_base.sum()/gencov_allbase*100
     print('Percentage of bases enriched in the target regions: {0:.2f}%'.format(Per_base))
-    return(ontag_base)
+    return(ontag_base, tagcov_base)
 
-ontag_base = Base_enrichment(temp,bam,target)
+ontag_base, tagcov_base = Base_enrichment(temp,bam,target)
 
 # =============================================================================
 # Q4. Uniformity of coverage 
 # =============================================================================
 
-total_tag_base = np.array([region.length for region in target])
-Mean_cov = ontag_base.sum()/total_tag_base.sum()              ## Mean coverage of target regions
-condition = 0.2*Mean_cov             
-lager_02mean = np.where(ontag_base > condition, 1, 0)
-uf = lager_02mean.sum()/lager_02mean.shape[0]*100             ## Uniformiity of coverage
+def Uniformity_cov(ontag_base):
+    total_tag_base = np.array([region.length for region in target])
+    Mean_cov = ontag_base.sum()/total_tag_base.sum()              ## Mean coverage of target regions
+    condition = 0.2*Mean_cov             
+    lager_02mean = np.where(ontag_base > condition, 1, 0)
+    uc = lager_02mean.sum()/lager_02mean.shape[0]*100             ## Uniformiity of coverage
+    
+    print('Uniformity of coverage: {0:.2f}%'.format(uc))
+    return(Mean_cov, total_tag_base)
 
-print('Uniformity of coverage: {0:.2f}%'.format(uf))
+Mean_cov, total_tag_base = Uniformity_cov(ontag_base)
 
 # =============================================================================
 # Q5. Mean coverage for each base in all target regions
@@ -113,27 +117,28 @@ print('Mean coverage for each base in all target regions: {0:.2f}'.format(Mean_c
 # Q6. An output tab-delimited txt file
 # =============================================================================
 
-region_base = py.bedcov(path_bed, path_bam).split('\n')      ## Total base depth per tag-region
-region_base.pop(-1)    
-region_base = pan.DataFrame([line.split('\t') for line in region_base], columns = ['Chromosome', 'Start', 'Stop', 'RegionID', 'MeanCoverage'])
-region_base.iloc[:, 4] = region_base.iloc[:, 4].map(lambda x: int(x))/total_tag_base
-region_base = region_base.reset_index()
-region_base = region_base.set_index('RegionID')
+def Output(path_bed, path_bam, tagcov_base, total_tag_base):
+    region_base = py.bedcov(path_bed, path_bam).split('\n')      ## Total base depth per tag-region
+    region_base.pop(-1)    
+    region_base = pan.DataFrame([line.split('\t') for line in region_base], columns = ['Chromosome', 'Start', 'Stop', 'RegionID', 'MeanCoverage'])
+    region_base.iloc[:, 4] = region_base.iloc[:, 4].map(lambda x: int(x))/total_tag_base
+    region_base = region_base.reset_index()
+    region_base = region_base.set_index('RegionID')
+    
+    # depth of each positon in target region
+    region_perbase = pan.DataFrame([([line[i] for i in [3,5]]) for line in tagcov_base], columns =['RegionID', 'Depth']) ## base depth on target region
+    region_perbase.iloc[:, 1] = region_perbase.iloc[:, 1].map(int) 
+    # Compute Std
+    StdCov = region_perbase.groupby(region_perbase['RegionID']).std() ## StdDev
+    StdCov.columns = ['StdDevCoverage']
+    # Merge two dataframe
+    df_list = [region_base, StdCov] 
+    output = pan.concat(df_list, axis = 1,ignore_index = False).sort_values('index')
+    output = output.reset_index()
+    reg_col = output.pop('level_0')
+    output.insert(4, 'RegionID', reg_col)
+    del output['index']
+    
+    output.to_csv('output.txt', index= None, sep='\t', float_format = '%.2f')
 
-
-region_perbase = pan.DataFrame([([line[i] for i in [3,5]]) for line in tagcov_base], columns =['RegionID', 'Depth']) ## base depth on target region
-region_perbase.iloc[:, 1] = region_perbase.iloc[:, 1].map(int) 
-# Compute Std
-StdCov = region_perbase.groupby(region_perbase['RegionID']).std() ## StdDev
-StdCov.columns = ['StdDevCoverage']
-# Merge two dataframe
-df_list = [region_base, StdCov] 
-output = pan.concat(df_list, axis = 1,ignore_index = False).sort_values('index')
-output = output.reset_index()
-reg_col = output.pop('level_0')
-output.insert(4, 'RegionID', reg_col)
-del output['index']
-
-output.to_csv('output.txt', index= None, sep='\t', float_format = '%.2f')
-
-
+Output(path_bed, path_bam, tagcov_base, total_tag_base)
